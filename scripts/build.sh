d8dd5cd75ecfc0a16548f9bb947191f1d76ad41a
theFAYRAS <24656477+fayras@users.noreply.github.com>
2023-08-20 19:20:10 +0200

    Update build.sh

--- scripts/build.sh
+++ scripts/build.sh
@@ -4,5 +4,5 @@ cd $(dirname "${BASH_SOURCE[0]}")
 echo "${BASH_SOURCE[0]}"
 set -euxo pipefail
 
-CGO_ENABLED=0 go build -o ./envbuilder ../cmd/envbuilder
+CGO_ENABLED=0 GOOS=linux go build -o ./envbuilder ../cmd/envbuilder
 # docker build -t envbuilder:latest .
